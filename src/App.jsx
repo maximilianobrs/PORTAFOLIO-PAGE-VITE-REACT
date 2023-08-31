9e5b707480355e864a7c212a40c59188ec01207a
maximilianobrs <maximilianonox@hotmail.com>
2023-08-30 20:35:43 -0400

    cambios estilos

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { Header } from "./components/Header"
 import { Main } from "./components/Main"
 import { Footer } from "./components/Footer"
 import { Aside } from "./components/Aside"
+import { About } from "./components/About"
 import './App.css'
 
 function App() {
   return (
     <>
       <Header />
-      <Aside/>
+      <About />
+      <Aside />
       <Main />
-      <Footer/>
+      <Footer />
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,9 @@ export function Header() {
         <header id='Home'>
             <div className="container" >
                 <div className='box-content-header'>
-                    <div className='box-about'>
+                    <div className='box-tittle-header'>
                         <h1>MAXIMILIANO BRIONES</h1>
                         <h2>DESARROLLADOR FULL STACK</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad voluptate architecto impedit cum omnis, velit nulla laudantium eos tempora error optio incidunt ipsam, nostrum corrupti, natus fuga vitae explicabo placeat.</p>
                         <div>
                             <ul>
                                 <li><a className='Icon-href' href="https://github.com/maximilianobrs" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="github" /></a></li>
