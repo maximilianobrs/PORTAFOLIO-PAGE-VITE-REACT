73c8ebc4b1020205ef1e2d0412b44657b8495b8d
maximilianobrs <maximilianonox@hotmail.com>
2023-08-31 16:42:19 -0400

    cambios imagenes

---
+++ src/assets/img/imagenes.js
@@ -0,0 +1,33 @@
+import javascript from  './javascript.png'
+import html from './html.png'
+import css from './css.png'
+import node from './nodejs.png'
+import express from './express.png'
+import posgresql from './postgresql.png'
+import sequelize from './sequelize.png'
+import vitejs from './vitejs.png'
+import bootstrap from   './bootstrap.png'
+import git from './git.png'
+import github from './github.png'
+import linkedin from './linkedin.png'
+import lupa from './lupa.png'
+import estrella from   './estrella.png'
+import cohete from './cohete.png'
+
+export const img = {
+    javascript,
+    html,
+    css,
+    node,
+    express,
+    posgresql,
+    sequelize,
+    vitejs,
+    bootstrap,
+    git,
+    github,
+    linkedin,
+    lupa,
+    estrella,
+    cohete
+}
